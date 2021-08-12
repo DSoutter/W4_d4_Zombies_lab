bb82263193076e23062e5ec8616af9021f8a1dde
DSoutter <duncan.soutter@gmail.com>
2021-08-12 14:43:59 +0100

    select all added

--- repositories/biting_repository.py
+++ repositories/biting_repository.py
@@ -2,8 +2,9 @@ from db.run_sql import run_sql
 from models.human import Human
 from models.zombie import Zombie
 from models.zombie_type import ZombieType
-import repositories.zombie_type_repository as zombie_type_repository
-
+from models.biting import Biting
+import repositories.zombie_repository as zombie_repository
+import repositories.human_repository as human_repository
     # def save(zombie):
     #     sql = "INSERT INTO zombies (name, zombie_type_id) VALUES (%s, %s) RETURNING id"
     #     values = [zombie.name, zombie.zombie_type.id]
@@ -29,6 +30,16 @@ def save(biting):
     #         zombies.append(zombie)
     #     return zombies
 
+def select_all():
+    bitings = []
+    sql = "SELECT * FROM bitings"
+    results = run_sql(sql)
+    for row in results:
+        human = human_repository.select(row['human_id'])
+        zombie = zombie_repository.select(row['zombie_id'])
+        biting = Biting(zombie, human, row['id'])
+        bitings.append(biting)
+    return bitings
 
     # def select(id):
     #     sql = "SELECT * FROM zombies WHERE id = %s"
@@ -37,6 +48,8 @@ def save(biting):
     #     zombie_type = zombie_type_repository.select(result["zombie_type_id"])
     #     zombie = Zombie(result["name"], zombie_type, result["id"])
     #     return zombie
+
+
 
 
     # def delete_all():
