f0fc9adf39c0797157bbc8df61c161a92ffb19b1
DSoutter <duncan.soutter@gmail.com>
2021-08-12 14:24:35 +0100

    bitings added

--- controllers/bitings_controller.py
+++ controllers/bitings_controller.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
+from models.biting import Biting
+import repositories.zombie_repository as zombie_repository
+import repositories.human_repository as human_repository
+import repositories.biting_repository as biting_repository
+
+
+
 bitings_blueprint = Blueprint("bitings", __name__)
 
 # INDEX
+
+@bitings_blueprint.route("/bitings", __name__)
+def bitings():
+    bitings = biting_repository.select_all()
+    return render_template("bitings/index.html", bitings = bitings)
 
 # NEW
 
--- repositories/biting_repository.py
+++ repositories/biting_repository.py
@@ -4,47 +4,53 @@ from models.zombie import Zombie
 from models.zombie_type import ZombieType
 import repositories.zombie_type_repository as zombie_type_repository
 
-# def save(zombie):
-#     sql = "INSERT INTO zombies (name, zombie_type_id) VALUES (%s, %s) RETURNING id"
-#     values = [zombie.name, zombie.zombie_type.id]
-#     results = run_sql(sql, values)
-#     id = results[0]['id']
-#     zombie.id = id
+    # def save(zombie):
+    #     sql = "INSERT INTO zombies (name, zombie_type_id) VALUES (%s, %s) RETURNING id"
+    #     values = [zombie.name, zombie.zombie_type.id]
+    #     results = run_sql(sql, values)
+    #     id = results[0]['id']
+    #     zombie.id = id
 
 def save(biting):
-
-# def select_all():
-#     zombies = []
-#     sql = "SELECT * FROM zombies"
-#     results = run_sql(sql)
-#     for result in results:
-#         zombie_type = zombie_type_repository.select(result["zombie_type_id"])
-#         zombie = Zombie(result["name"], zombie_type, result["id"])
-#         zombies.append(zombie)
-#     return zombies
+    sql = "INSERT INTO bitings (human_id, zombie_id) VALUES (%s, %s) RETURNING id"
+    values = [biting.human.id, biting.zombie.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    biting.id = id
 
 
-# def select(id):
-#     sql = "SELECT * FROM zombies WHERE id = %s"
-#     values = [id]
-#     result = run_sql(sql, values)[0]
-#     zombie_type = zombie_type_repository.select(result["zombie_type_id"])
-#     zombie = Zombie(result["name"], zombie_type, result["id"])
-#     return zombie
+    # def select_all():
+    #     zombies = []
+    #     sql = "SELECT * FROM zombies"
+    #     results = run_sql(sql)
+    #     for result in results:
+    #         zombie_type = zombie_type_repository.select(result["zombie_type_id"])
+    #         zombie = Zombie(result["name"], zombie_type, result["id"])
+    #         zombies.append(zombie)
+    #     return zombies
 
 
-# def delete_all():
-#     sql = "DELETE FROM zombies"
-#     run_sql(sql)
+    # def select(id):
+    #     sql = "SELECT * FROM zombies WHERE id = %s"
+    #     values = [id]
+    #     result = run_sql(sql, values)[0]
+    #     zombie_type = zombie_type_repository.select(result["zombie_type_id"])
+    #     zombie = Zombie(result["name"], zombie_type, result["id"])
+    #     return zombie
 
 
-# def delete(id):
-#     sql = "DELETE FROM zombies WHERE id = %s"
-#     values = [id]
-#     run_sql(sql, values)
+    # def delete_all():
+    #     sql = "DELETE FROM zombies"
+    #     run_sql(sql)
 
 
-# def update(zombie):
-#     sql = "UPDATE zombies SET (name, zombie_type_id) = (%s, %s) WHERE id = %s"
-#     values = [zombie.name, zombie.zombie_type.id, zombie.id]
-#     run_sql(sql, values)
+    # def delete(id):
+    #     sql = "DELETE FROM zombies WHERE id = %s"
+    #     values = [id]
+    #     run_sql(sql, values)
+
+
+    # def update(zombie):
+    #     sql = "UPDATE zombies SET (name, zombie_type_id) = (%s, %s) WHERE id = %s"
+    #     values = [zombie.name, zombie.zombie_type.id, zombie.id]
+    #     run_sql(sql, values)
